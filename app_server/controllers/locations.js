682e109b8eeab16ee722a47245add094eb225f3f
proes0619 <104222288+proes0619@users.noreply.github.com>
2024-11-07 22:41:03 +0900

    Add files via upload

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -4,7 +4,7 @@ const apiOptions = {
   server: 'http://localhost:3000'
 };
 if(process.env.NODE_ENV === 'production'){
-  apiOptions.server  = 'https://YOUR_API.onrender.com';
+  apiOptions.server  = 'https://loc8r-api-9qwb.onrender.com';
 }
 
 const homelist = (req, res) => {
