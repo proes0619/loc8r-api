d06de5de54a08488e739dc2282f2c702561d889a
proes0619 <104222288+proes0619@users.noreply.github.com>
2024-12-08 22:44:15 +0900

    Add files via upload

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -4,7 +4,7 @@ const apiOptions = {
   server: 'http://localhost:3000'
 };
 if(process.env.NODE_ENV === 'production'){
-  apiOptions.server  = 'https://calm-khapse-6ebbfe.netlify.app/';
+  apiOptions.server  = 'https://loc8r-api-9qwb.onrender.com';
 }
 
 const homelist = (req, res) => {
